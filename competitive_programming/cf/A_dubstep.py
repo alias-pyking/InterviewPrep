e196e8cda0462d7b89bf9d557b87e406a15664dd
shubhamsks <shubhamkumar7051@gmail.com>
2020-05-04 22:41:59 +0530

    a little change in directories

--- competitive_programming/cf/A_dubstep.py
+++
@@ -1,6 +0,0 @@
-if __name__ == "__main__":
-	inp = str(input())
-	list = inp.split('WUB')
-	list = [x for x in list if x != '']
-	print(*list)
-	
--- competitive_programming/cf/anton_and_letters.py
+++
@@ -1,3 +0,0 @@
-letters = str(input())
-letter_set = set([x for x in letters if x not in ['{', '}',' ',',']])
-print(len(letter_set))
